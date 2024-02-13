e0e4bd62a6db5a3183cb4b92551b2e258c388a7d
1nv0k32 <armin.mahdilou@gmail.com>
2024-02-13 23:06:38 +0100

    2024/02/08-22:32:18

--- system/wsl.nix
+++ system/wsl.nix
@@ -1,32 +1,6 @@
 { pkgs, lib, options, ... }:
 with lib;
 let
-  alpaca = pkgs.stdenv.mkDerivation rec {
-    pname = "alpaca";
-    version = "2.0.3";
-    src = pkgs.fetchurl {
-      url = "https://github.com/samuong/alpaca/releases/download/v${version}/alpaca_v${version}_linux-amd64";
-      sha256 = "HFT6URkSOtJLKcCRs/epHFeLgZxVh5YEYJJtl74MokM=";
-    };
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-    phases = [ "installPhase" ];
-    installPhase = ''
-      install -m755 -D $src $out/bin/${pname}
-      autoPatchelf $out/bin/${pname}
-    '';
-  };
-
-  proxy = "http://localhost:3128";
-  proxies = {
-    no_proxy = "127.0.0.1,localhost";
-    ftp_proxy = proxy;
-    https_proxy = proxy;
-    HTTPS_PROXY = proxy;
-    http_proxy = proxy;
-    HTTP_PROXY = proxy;
-    all_proxy = proxy;
-    rsync_proxy = proxy;
-  };
 in
 {
   imports = [
@@ -45,30 +19,12 @@ in
 
   systemd.services = {
     systemd-resolved.enable = mkForce false;
-    "alpaca" = {
-      enable = true;
-      description = "alpaca proxy service";
-      serviceConfig = {
-        ExecStart = "${alpaca}/bin/alpaca -l 0.0.0.0 -p 3128 $PACURL";
-        Restart = "always";
-        KillMode = "mixed";
-      };
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-      environment = {
-        NTLM_CREDENTIALS = mkDefault "NTLM";
-        PACURL = mkDefault "-C PROXYURL";
-      };
-    };
-    nix-daemon.environment = proxies;
-    k3s.environment = proxies;
   };
 
   boot.loader.systemd-boot.enable = mkForce false;
   networking = {
     networkmanager.enable = mkForce false;
     firewall.enable = mkForce false;
-    proxy.default = mkForce proxy;
   };
 
   virtualisation.docker.daemon.settings = {
@@ -82,3 +38,4 @@ in
 }
 
 # vim:expandtab ts=2 sw=2
+
