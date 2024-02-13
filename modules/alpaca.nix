0f54a03bbf2aa0b8734f6c154bc3b8e91cdfe2b5
1nv0k32 <armin.mahdilou@gmail.com>
2024-02-13 23:06:38 +0100

    2024/02/08-22:32:58

--- modules/alpaca.nix
+++ modules/alpaca.nix
@@ -15,68 +15,68 @@ in
     };
     listenAddr = {
       type = types.str;
-      default "0.0.0.0";
-      };
-      listenPort = {
-        type = types.str;
-        default = "3128"
-          };
-
-        config = mkIf cfg.enable let
-        proxy = "http://${cfg.listenAddr}:${cfg.listenPort}";
-        proxies = {
-          no_proxy = "127.0.0.1,localhost";
-          ftp_proxy = proxy;
-          https_proxy = proxy;
-          HTTPS_PROXY = proxy;
-          http_proxy = proxy;
-          HTTP_PROXY = proxy;
-          all_proxy = proxy;
-          rsync_proxy = proxy;
-        };
-        alpaca = pkgs.stdenv.mkDerivation rec {
-          pname = "alpaca";
-          version = "2.0.3";
-          src = pkgs.fetchurl {
-            url = "https://github.com/samuong/alpaca/releases/download/v${version}/alpaca_v${version}_linux-amd64";
-            sha256 = "HFT6URkSOtJLKcCRs/epHFeLgZxVh5YEYJJtl74MokM=";
-          };
-          nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-          phases = [ "installPhase" ];
-          installPhase = ''
-            install -m755 -D $src $out/bin/${pname}
-            autoPatchelf $out/bin/${pname}
-          '';
-        };
-        pac_arg = "" + optionalString (cfg.pacUrl != null) "-C ${cfg.pacUrl}";
-        in
-        {
-        # Setup service
-        systemd.services.alpaca = {
-          enable = true;
-          description = "alpaca proxy service";
-          serviceConfig = {
-            ExecStart = "${alpaca}/bin/alpaca -l ${cfg.listenAddr} -p ${cfg.listenPort} ${pac_arg}";
-            Restart = "always";
-            KillMode = "mixed";
-          };
-          after = [ "network.target" ];
-          wantedBy = [ "multi-user.target" ];
-          environment = {
-            NTLM_CREDENTIALS = "${cfg.ntlmHash}";
-          };
+      default = "0.0.0.0";
+    };
+    listenPort = {
+      type = types.str;
+      default = "3128"
         };
 
-        # Set proxy on system and services
-        systemd.services = {
-          nix-daemon.environment = proxies;
-          k3s.environment = proxies;
+      config = mkIf cfg.enable let
+      proxy = "http://${cfg.listenAddr}:${cfg.listenPort}";
+      proxies = {
+        no_proxy = "127.0.0.1,localhost";
+        ftp_proxy = proxy;
+        https_proxy = proxy;
+        HTTPS_PROXY = proxy;
+        http_proxy = proxy;
+        HTTP_PROXY = proxy;
+        all_proxy = proxy;
+        rsync_proxy = proxy;
+      };
+      alpaca = pkgs.stdenv.mkDerivation rec {
+        pname = "alpaca";
+        version = "2.0.3";
+        src = pkgs.fetchurl {
+          url = "https://github.com/samuong/alpaca/releases/download/v${version}/alpaca_v${version}_linux-amd64";
+          sha256 = "HFT6URkSOtJLKcCRs/epHFeLgZxVh5YEYJJtl74MokM=";
         };
-        networking = {
-          proxy.default = mkForce proxy;
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+        phases = [ "installPhase" ];
+        installPhase = ''
+          install -m755 -D $src $out/bin/${pname}
+          autoPatchelf $out/bin/${pname}
+        '';
+      };
+      pac_arg = "" + optionalString (cfg.pacUrl != null) "-C ${cfg.pacUrl}";
+      in
+      {
+      # Setup service
+      systemd.services.alpaca = {
+        enable = true;
+        description = "alpaca proxy service";
+        serviceConfig = {
+          ExecStart = "${alpaca}/bin/alpaca -l ${cfg.listenAddr} -p ${cfg.listenPort} ${pac_arg}";
+          Restart = "always";
+          KillMode = "mixed";
+        };
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        environment = {
+          NTLM_CREDENTIALS = "${cfg.ntlmHash}";
         };
       };
-    }
+
+      # Set proxy on system and services
+      systemd.services = {
+        nix-daemon.environment = proxies;
+        k3s.environment = proxies;
+      };
+      networking = {
+        proxy.default = mkForce proxy;
+      };
+    };
+  }
 
 # vim:expandtab ts=2 sw=2
 
