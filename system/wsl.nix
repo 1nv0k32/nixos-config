e6adf13fec26834ab5cbb78a1658f355465c2d95
1nv0k32 <armin.mahdilou@gmail.com>
2024-02-14 14:59:47 +0100

    2024/02/14-14:59:47

--- system/wsl.nix
+++ system/wsl.nix
@@ -44,7 +44,7 @@ with lib;
     description = "tmux background service";
     serviceConfig = {
       Type = "forking";
-      ExecStart = "${pkgs.bash} -c '${pkgs.tmux}/bin/tmux new-session -d -s background || true'";
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.tmux}/bin/tmux new-session -d -s background || true'";
     };
     wantedBy = [ "multi-user.target" ];
   };
