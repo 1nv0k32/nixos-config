1be273b818af10b92d9e3557201a836bcbc0219c
1nv0k32 <armin.mahdilou@gmail.com>
2024-04-04 03:35:59 +0200

    2024/04/04-03:35:59

--- modules/cert_der.nix
+++ modules/cert_der.nix
@@ -38,7 +38,7 @@ in
             }
           );
       in
-      options.security.pki.certificateFiles.default ++ [ "${certs}/cert/rootCore.crt" ];
+      options.security.pki.certificateFiles.default ++ [ "${builtins.elemAt certs 0}/cert/rootCore.crt" ];
   };
 }
 
