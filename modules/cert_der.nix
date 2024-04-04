7ea8ec2fcd701ad281a0a2355ea5d1d2ac253df6
1nv0k32 <armin.mahdilou@gmail.com>
2024-04-04 02:34:38 +0200

    2024/04/04-02:34:38

--- modules/cert_der.nix
+++ modules/cert_der.nix
@@ -1,11 +1,9 @@
 { lib, pkgs, config, options, ... }:
 with lib;
 let
-  cfg = config.security.pki.certDER;
-in
-{
-  options.security.pki.certDER = {
-    enable = mkEnableOption "cert DER";
+  cfg = config.security.pki;
+
+  certOpts = { name, ... }: {
     name = mkOption {
       type = types.str;
     };
@@ -16,25 +14,31 @@ in
       type = types.str;
     };
   };
+in
+{
+  options.security.pki.certDER = mkOption {
+    default = { };
+    type = with types; attrsOf (submodule certOpts);
+  };
 
-  config = mkIf cfg.enable {
-    security.pki.certificateFiles =
-      let
-        cert = pkgs.stdenv.mkDerivation rec {
-          name = cfg.name;
-          src = pkgs.fetchurl {
-            url = cfg.url;
-            hash = cfg.hash;
-          };
-          nativeBuildInputs = [ pkgs.openssl ];
-          phases = [ "installPhase" ];
-          installPhase = ''
-            install -m644 -D $src $out/cert/${name}.der
-            openssl x509 -inform der -in $out/cert/${name}.der -out $out/cert/${name}.crt
-          '';
-        };
-      in
-      options.security.pki.certificateFiles.default ++ [ "${cert}/cert/${cfg.name}.crt" ];
+  config = {
+    #security.pki.certificateFiles =
+    #  let
+    #    cert = pkgs.stdenv.mkDerivation rec {
+    #      name = cfg.name;
+    #      src = pkgs.fetchurl {
+    #        url = cfg.url;
+    #        hash = cfg.hash;
+    #      };
+    #      nativeBuildInputs = [ pkgs.openssl ];
+    #      phases = [ "installPhase" ];
+    #      installPhase = ''
+    #        install -m644 -D $src $out/cert/${name}.der
+    #        openssl x509 -inform der -in $out/cert/${name}.der -out $out/cert/${name}.crt
+    #      '';
+    #    };
+    #  in
+    #  options.security.pki.certificateFiles.default ++ [ "${cert}/cert/${cfg.name}.crt" ];
   };
 }
 
