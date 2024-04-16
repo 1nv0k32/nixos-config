83d982070567b4dfdbc084cf0c596b7ad62a3071
1nv0k32 <armin.mahdilou@gmail.com>
2024-04-16 14:39:45 +0200

    2024/04/16-14:39:45

--- system/wsl.nix
+++ system/wsl.nix
@@ -14,6 +14,7 @@ with lib;
       { src = "${coreutils}/bin/readlink"; }
       { src = "${wget}/bin/wget"; }
       { src = "${curl}/bin/curl"; }
+      { src = "${getconf}/bin/getconf"; }
     ];
   };
   boot.loader.systemd-boot.enable = mkForce false;
