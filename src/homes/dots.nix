7089b6b9120a224052486aadb22ac2d66f4dc16c
Armin <Armin.Mahdilou@gmail.com>
2024-04-24 23:27:52 +0200

    2024/04/24-23:27:52

--- src/homes/dots.nix
+++ src/homes/dots.nix
@@ -26,6 +26,11 @@ with lib;
       done
     )
 
+    if command -v fzf-share >/dev/null; then
+      source "$(fzf-share)/key-bindings.bash"
+      source "$(fzf-share)/completion.bash"
+    fi
+
     if test -f ~/.bashrc.local; then
     . ~/.bashrc.local
     fi
