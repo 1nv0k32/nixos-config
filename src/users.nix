9d23684f37208e36cd26e5e1039cdd9dfe4fd318
Armin <Armin.Mahdilou@gmail.com>
2024-05-26 22:24:20 +0200

    2024/05/26-22:24:20

--- src/users.nix
+++ src/users.nix
@@ -8,8 +8,8 @@
 }:
 let
   mainUser = config.environment.sysConf.mainUser;
-  userPkgs = (import ../pkgs/gnome-ext.nix { inherit pkgs; }).userPkgs;
-  gnomeExtensions = (import ../pkgs/user.nix { inherit pkgs; }).gnomeExtensions;
+  userPkgs = (import ../pkgs/user.nix { inherit pkgs; }).userPkgs;
+  gnomeExtensions = (import ../pkgs/gnome-ext.nix { inherit pkgs; }).gnomeExtensions;
 in
 with lib;
 {
