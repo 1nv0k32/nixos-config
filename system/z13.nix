8ec001fc26a3d408667d4aaf0daaf2bb26a87c5f
Armin <Armin.Mahdilou@gmail.com>
2024-05-31 01:23:55 +0200

    2024/05/31-01:23:55

--- system/z13.nix
+++ system/z13.nix
@@ -24,37 +24,39 @@ let
 in
 with lib;
 {
-  imports = [
-    (import ../src/extra.nix)
-    (import ../pkgs/extra.nix)
-  ];
-
   # boot.initrd.luks.devices."root".crypttabExtraOpts = [ "tpm2-device=auto" ];
   options.boot.initrd.luks.devices = mkOption {
     type = types.attrsOf (types.submodule devicesModule);
   };
 
-  services = {
-    tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 90;
-        STOP_CHARGE_THRESH_BAT0 = 99;
-      };
-    };
-    power-profiles-daemon.enable = mkForce false;
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        "charger" = {
-          governor = "performance";
-          turbo = "always";
+  config = {
+    imports = [
+      (import ../src/extra.nix)
+      (import ../pkgs/extra.nix)
+    ];
+
+    services = {
+      tlp = {
+        enable = true;
+        settings = {
+          START_CHARGE_THRESH_BAT0 = 90;
+          STOP_CHARGE_THRESH_BAT0 = 99;
         };
-        "battery" = {
-          governor = "ondemand";
-          scaling_min_freq = 400000;
-          scaling_max_freq = 1600000;
-          turbo = "never";
+      };
+      power-profiles-daemon.enable = mkForce false;
+      auto-cpufreq = {
+        enable = true;
+        settings = {
+          "charger" = {
+            governor = "performance";
+            turbo = "always";
+          };
+          "battery" = {
+            governor = "ondemand";
+            scaling_min_freq = 400000;
+            scaling_max_freq = 1600000;
+            turbo = "never";
+          };
         };
       };
     };
