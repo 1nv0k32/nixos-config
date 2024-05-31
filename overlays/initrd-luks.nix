865f10476de6b0ecfcc9ca557e73c17d1d4fe7c6
Armin <Armin.Mahdilou@gmail.com>
2024-05-31 05:50:27 +0200

    2024/05/31-05:50:27

---
+++ overlays/initrd-luks.nix
@@ -0,0 +1,16 @@
+{ config, lib, ... }:
+with lib;
+{
+  options.boot.initrd.luks.devices = mkOption {
+    type = types.attrsOf (
+      types.submodule {
+        config.crypttabExtraOpts = mkDefault [
+          "tpm2-device=auto"
+          "nofail"
+        ];
+      }
+    );
+  };
+}
+
+# vim:expandtab ts=2 sw=2
--- system/z13.nix
+++ system/z13.nix
@@ -1,53 +1,39 @@
 {
   inputs,
   system,
-  config,
   pkgs,
   lib,
   ...
 }:
 with lib;
 {
-  options.boot.initrd.luks.devices = mkOption {
-    type = types.attrsOf (
-      types.submodule {
-        config.crypttabExtraOpts = mkDefault [
-          "tpm2-device=auto"
-          "nofail"
-        ];
-      }
-    );
-  };
-
   imports = [
     (import ../src/extra.nix)
     (import ../pkgs/extra.nix)
+    (import ../overlays/initrd-luks.nix)
   ];
 
-  config = {
-
-    services = {
-      tlp = {
-        enable = true;
-        settings = {
-          START_CHARGE_THRESH_BAT0 = 90;
-          STOP_CHARGE_THRESH_BAT0 = 99;
-        };
+  services = {
+    tlp = {
+      enable = true;
+      settings = {
+        START_CHARGE_THRESH_BAT0 = 90;
+        STOP_CHARGE_THRESH_BAT0 = 99;
       };
-      power-profiles-daemon.enable = mkForce false;
-      auto-cpufreq = {
-        enable = true;
-        settings = {
-          "charger" = {
-            governor = "performance";
-            turbo = "always";
-          };
-          "battery" = {
-            governor = "ondemand";
-            scaling_min_freq = 400000;
-            scaling_max_freq = 1600000;
-            turbo = "never";
-          };
+    };
+    power-profiles-daemon.enable = mkForce false;
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        "charger" = {
+          governor = "performance";
+          turbo = "always";
+        };
+        "battery" = {
+          governor = "ondemand";
+          scaling_min_freq = 400000;
+          scaling_max_freq = 1600000;
+          turbo = "never";
         };
       };
     };
