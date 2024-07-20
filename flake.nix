f703f8c696c1a1f567e24e776c57aedfd1356d4d
Armin <Armin.Mahdilou@gmail.com>
2024-07-20 02:24:11 +0200

    2024/07/20-02:24:11

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/2405.5.4";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,12 +19,13 @@
   };
 
   outputs =
-    { self, home-manager, ... }@inputs:
+    { self, ... }@inputs:
     {
       stateVersion = "24.05";
       system = "x86_64-linux";
       baseModules = [
-        home-manager.nixosModules.home-manager
+        inputs.home-manager.nixosModules.home-manager
+        inputs.nixvim.nixosModules.nixvim
         (import "${self}/modules")
         (import "${self}/src/base.nix")
         (import "${self}/pkgs/base.nix")
