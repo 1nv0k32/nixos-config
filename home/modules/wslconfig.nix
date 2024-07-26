351bce8e71db3da51415e3d618746c6041ebd7bf
Armin <Armin.Mahdilou@gmail.com>
2024-07-27 01:06:24 +0200

    Refactor wslconfig service

---
+++ home/modules/wslconfig.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+{
+  systemd.user.services.wslconfig = {
+    Unit.Description = "Automatically set .wslconfig in current windows user";
+    Install.WantedBy = [ "default.target" ];
+    Service.ExecStart = "${pkgs.writeShellScript "wslconfig-sh" ''
+      #!${pkgs.bash}/bin/bash
+      set -e
+      CURRENT_USER=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe '$env:UserName')
+      CURRENT_USER=''${CURRENT_USER//[^a-zA-Z0-9]/}
+      ${pkgs.coreutils}/bin/cat << EOF > /mnt/c/Users/$CURRENT_USER/.wslconfig
+      [wsl2]
+      kernelCommandLine = vsyscall=emulate cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1
+      networkingMode=mirrored
+      autoProxy=false
+      firewall=false
+      [experimental]
+      sparseVhd=true
+      EOF
+    ''}";
+  };
+}
+
+# vim:expandtab ts=2 sw=2
--- system/wsl.nix
+++ system/wsl.nix
@@ -78,27 +78,10 @@ with lib;
     "${mainUser}" =
       { ... }:
       {
-        imports = [ (import "${inputs.vscode-server}/modules/vscode-server/home.nix") ];
-
-        systemd.user.services.wslconfig = {
-          Unit.Description = "Automatically set .wslconfig in current windows user";
-          Install.WantedBy = [ "default.target" ];
-          Service.ExecStart = "${pkgs.writeShellScript "wslconfig-sh" ''
-            #!${pkgs.bash}/bin/bash
-            set -e
-            CURRENT_USER=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe '$env:UserName')
-            CURRENT_USER=''${CURRENT_USER//[^a-zA-Z0-9]/}
-            ${pkgs.coreutils}/bin/cat << EOF > /mnt/c/Users/$CURRENT_USER/.wslconfig
-            [wsl2]
-            kernelCommandLine = vsyscall=emulate cgroup_no_v1=all systemd.unified_cgroup_hierarchy=1
-            networkingMode=mirrored
-            autoProxy=false
-            firewall=false
-            [experimental]
-            sparseVhd=true
-            EOF
-          ''}";
-        };
+        imports = [
+          (import "${inputs.vscode-server}/modules/vscode-server/home.nix")
+          (import ../home/modules/wslconfig.nix)
+        ];
       };
   };
 }
