0fcc87642b3d680e4cbe6e10c3cbccdb519e866c
1nv0k32 <armin.mahdilou@gmail.com>
2024-07-26 17:18:21 +0200

    2024/07/26-17:18:21

--- system/wsl.nix
+++ system/wsl.nix
@@ -84,6 +84,7 @@ with lib;
           Unit.Description = "Automatically set .wslconfig in current windows user";
           Install.WantedBy = [ "default.target" ];
           Service.ExecStart = "${pkgs.writeShellScript "wslconfig-sh" ''
+            #!${pkgs.bash}/bin/bash
             set -e
             CURRENT_USER=$(/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe '$env:UserName')
             CURRENT_USER=''${CURRENT_USER//[^a-zA-Z0-9]/}
