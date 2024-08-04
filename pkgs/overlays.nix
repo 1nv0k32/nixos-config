e0a3a23e69c21f937191372a3de990e41f79370b
Armin <Armin.Mahdilou@gmail.com>
2024-08-04 03:52:07 +0200

    2024/08/04-03:52:07

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -1,23 +1,21 @@
 { inputs, system, ... }:
 let
-  overlays = {
-    pkgs-master = _: prev: {
-      pkgs-master = import (inputs.nixpkgs-master) {
-        inherit (prev.stdenv) system;
-        config.allowUnfree = true;
-      };
+  pkgs-master = _: prev: {
+    pkgs-master = import (inputs.nixpkgs-master) {
+      inherit (prev.stdenv) system;
+      config.allowUnfree = true;
     };
-    pkgs-unstable = _: prev: {
-      pkgs-unstable = import (inputs.nixpkgs-unstable) {
-        inherit (prev.stdenv) system;
-        config.allowUnfree = true;
-      };
+  };
+  pkgs-unstable = _: prev: {
+    pkgs-unstable = import (inputs.nixpkgs-unstable) {
+      inherit (prev.stdenv) system;
+      config.allowUnfree = true;
     };
-    pkgs-old = _: prev: {
-      pkgs-old = import (inputs.nixpkgs-old) {
-        inherit (prev.stdenv) system;
-        config.allowUnfree = true;
-      };
+  };
+  pkgs-old = _: prev: {
+    pkgs-old = import (inputs.nixpkgs-old) {
+      inherit (prev.stdenv) system;
+      config.allowUnfree = true;
     };
   };
 in
@@ -25,9 +23,9 @@ in
   nixpkgs = {
     config.allowUnfree = true;
     overlays = [
-      overlays.pkgs-master
-      overlays.pkgs-unstable
-      overlays.pkgs-old
+      pkgs-master
+      pkgs-unstable
+      pkgs-old
     ];
   };
 }
