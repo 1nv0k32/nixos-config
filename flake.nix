35247327aa6df41562a34f06b6da7279eebaa1e8
Armin <Armin.Mahdilou@gmail.com>
2024-08-04 04:42:23 +0200

    2024/08/04-04:42:23

--- flake.nix
+++ flake.nix
@@ -24,16 +24,13 @@
 
   outputs =
     { self, ... }@inputs:
-    let
-      system = "x86_64-linux";
-    in
     {
-      inherit system;
+      system = "x86_64-linux";
       stateVersion = "24.05";
       baseModules = [
         inputs.home-manager.nixosModules.home-manager
         inputs.nixvim.nixosModules.nixvim
-        (import "${self}/pkgs/overlays.nix" { inherit inputs system; })
+        (import "${self}/pkgs/overlays.nix" { inherit inputs; })
         (import "${self}/modules")
         (import "${self}/src/base.nix")
         (import "${self}/pkgs/base.nix")
