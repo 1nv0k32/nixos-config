dd7018c404ed38a366858e89197f16ac5a0a7f64
Armin <Armin.Mahdilou@gmail.com>
2024-08-12 00:38:40 +0200

    2024/08/12-00:38:40

--- misc/flake.nix
+++ misc/flake.nix
@@ -8,7 +8,7 @@
   outputs =
     { user-config, ... }@inputs:
     let
-      baseModules = [
+      localModules = [
         (
           { lib, ... }:
           {
@@ -24,7 +24,7 @@
       nixosConfigurations = {
         "nyx" = user-config.inputs.nixpkgs.lib.nixosSystem user-config.systemTypes.z13g2 // {
           specialArgs.hostName = "nyx";
-          # modules = baseModules;
+          modules = localModules ++ user-config.systemTypes.z13g2.modules;
         };
       };
     };
