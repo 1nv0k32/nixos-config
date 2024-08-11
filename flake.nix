e850a56e07d3e5b545204cd85a3a890b2c2ed75f
Armin <Armin.Mahdilou@gmail.com>
2024-08-12 01:42:03 +0200

    2024/08/12-01:42:03

--- flake.nix
+++ flake.nix
@@ -34,9 +34,9 @@
             imports =
               [ ]
               ++ lib.optional (builtins.pathExists "${dir}/hardware-configuration.nix") (
-                import ${dir}/hardware-configuration.nix
+                import "${dir}/hardware-configuration.nix"
               )
-              ++ lib.optional (builtins.pathExists "${dir}/local.nix") (import ${dir}/local.nix);
+              ++ lib.optional (builtins.pathExists "${dir}/local.nix") (import "${dir}/local.nix");
           }
         )
       ];
