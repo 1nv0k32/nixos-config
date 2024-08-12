260231b63cb452fec9639e186315260292e76d85
Armin <Armin.Mahdilou@gmail.com>
2024-08-12 02:04:26 +0200

    2024/08/12-02:04:26

--- misc/flake.nix
+++ misc/flake.nix
@@ -9,19 +9,21 @@
     { cfg, ... }@inputs:
     let
       lib = cfg.inputs.nixpkgs.lib;
-      nyxCfg = cfg.systemTypes.z13g2 // {
-        specialArgs = cfg.systemTypes.z13g2.specialArgs // {
-          hostName = "nyx";
-        };
-        modules =
-          cfg.systemTypes.z13g2.modules
-          ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) (import ./hardware-configuration.nix)
-          ++ lib.optional (builtins.pathExists ./local.nix) (import ./local.nix);
-      };
     in
     {
       nixosConfigurations = {
-        "nyx" = lib.nixosSystem nyxCfg;
+        "nyx" = lib.nixosSystem (
+          cfg.systemTypes.z13g2
+          // {
+            specialArgs = cfg.systemTypes.z13g2.specialArgs // {
+              hostName = "nyx";
+            };
+            modules =
+              cfg.systemTypes.z13g2.modules
+              ++ lib.optional (builtins.pathExists ./hardware-configuration.nix) (import ./hardware-configuration.nix)
+              ++ lib.optional (builtins.pathExists ./local.nix) (import ./local.nix);
+          }
+        );
       };
     };
 }
