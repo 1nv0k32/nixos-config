112b112d801e33c2bd58527a135c617f6edb10e4
Armin <Armin.Mahdilou@gmail.com>
2024-08-13 03:17:07 +0200

    2024/08/13-03:17:07

--- system/z13g2.nix
+++ system/z13g2.nix
@@ -34,6 +34,8 @@ with lib;
         CPU_BOOST_ON_BAT = 0;
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
       };
     };
   };
