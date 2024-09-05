cc3707d2ac799ea9bb6a3f6ebe03eae8de7cff7b
Armin <Armin.Mahdilou@gmail.com>
2024-09-05 22:53:21 +0200

    2024/09/05-22:53:21

--- flake.nix
+++ flake.nix
@@ -1,4 +1,11 @@
 {
+  ixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-23.11";
