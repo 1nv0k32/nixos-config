8b7f6a844f5f0c206c069b703c188e3586de7da7
Armin <Armin.Mahdilou@gmail.com>
2024-09-06 03:13:17 +0200

    2024/09/06-03:13:17

--- system/rpi5.nix
+++ system/rpi5.nix
@@ -7,8 +7,6 @@ with lib;
       config = {
         all = {
           base-dt-params = {
-            # enable autoprobing of bluetooth driver
-            # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
             krnbt = {
               enable = true;
               value = "on";
