ac9acf66d48721d8d4c3db6a171554e85b022889
Armin <Armin.Mahdilou@gmail.com>
2024-09-08 23:33:39 +0200

    2024/09/08-23:33:39

--- system/rpi5.nix
+++ system/rpi5.nix
@@ -8,7 +8,7 @@ with lib;
 {
   users.users."${config.environment.sysConf.mainUser}".initialPassword = "rpi5";
   boot = {
-    kernelPackages = pkgs.pkgs-unstable.linuxPackages_rpi4;
+    kernelPackages = pkgs.linuxPackages_rpi4;
     initrd.availableKernelModules = [
       "nvme"
       "usbhid"
