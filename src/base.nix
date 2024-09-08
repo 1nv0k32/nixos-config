79b567feac5825367a67c4d99e4fe955af78f18c
Armin <Armin.Mahdilou@gmail.com>
2024-09-08 05:27:08 +0200

    2024/09/08-05:27:08

--- src/base.nix
+++ src/base.nix
@@ -57,109 +57,109 @@ with lib;
 
   networking = {
     hostName = hostName;
-    networkmanager = {
-      enable = true;
-      dns = "systemd-resolved";
-      settings = {
-        main = {
-          no-auto-default = "*";
-          systemd-resolved = true;
-        };
-      };
+    # networkmanager = {
+    #   enable = true;
+    #   dns = "systemd-resolved";
+    #   settings = {
+    #     main = {
+    #       no-auto-default = "*";
+    #       systemd-resolved = true;
+    #     };
+    #   };
+    # };
+  };
+
+  systemd = {
+    extraConfig = customConfigs.SYSTEMD_CONFIG;
+    user.extraConfig = customConfigs.SYSTEMD_USER_CONFIG;
+    watchdog = {
+      runtimeTime = "off";
+      rebootTime = "off";
+      kexecTime = "off";
     };
   };
 
-  # systemd = {
-  #   extraConfig = customConfigs.SYSTEMD_CONFIG;
-  #   user.extraConfig = customConfigs.SYSTEMD_USER_CONFIG;
-  #   watchdog = {
-  #     runtimeTime = "off";
-  #     rebootTime = "off";
-  #     kexecTime = "off";
-  #   };
-  # };
+  console = {
+    earlySetup = true;
+    packages = [ pkgs.terminus_font ];
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v24b.psf.gz";
+    keyMap = "us";
+  };
 
-  # console = {
-  #   earlySetup = true;
-  #   packages = [ pkgs.terminus_font ];
-  #   font = "${pkgs.terminus_font}/share/consolefonts/ter-v24b.psf.gz";
-  #   keyMap = "us";
-  # };
+  time = {
+    timeZone = "CET";
+    hardwareClockInLocalTime = false;
+  };
 
-  # time = {
-  #   timeZone = "CET";
-  #   hardwareClockInLocalTime = false;
-  # };
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = mkForce "1";
+    };
+    variables = {
+      VAGRANT_DEFAULT_PROVIDER = mkForce "libvirt";
+      LIBVIRT_DEFAULT_URI = mkForce "qemu:///system";
+    };
+    etc = {
+      "inputrc".text = customConfigs.INPUTRC_CONFIG;
+      "bashrc.local".text = customConfigs.BASHRC_CONFIG;
+      "wireplumber/policy.lua.d/99-bluetooth-policy.lua".text = ''
+        bluetooth_policy.policy["media-role.use-headset-profile"] = false
+      '';
+    };
+  };
 
-  # environment = {
-  #   sessionVariables = {
-  #     NIXOS_OZONE_WL = mkForce "1";
-  #   };
-  #   variables = {
-  #     VAGRANT_DEFAULT_PROVIDER = mkForce "libvirt";
-  #     LIBVIRT_DEFAULT_URI = mkForce "qemu:///system";
-  #   };
-  #   etc = {
-  #     "inputrc".text = customConfigs.INPUTRC_CONFIG;
-  #     "bashrc.local".text = customConfigs.BASHRC_CONFIG;
-  #     "wireplumber/policy.lua.d/99-bluetooth-policy.lua".text = ''
-  #       bluetooth_policy.policy["media-role.use-headset-profile"] = false
-  #     '';
-  #   };
-  # };
+  services = {
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+    };
+    resolved = {
+      enable = true;
+      extraConfig = customConfigs.RESOLVED_CONFIG;
+    };
+    logind =
+      let
+        defaultAction = "lock";
+        suspendAction = "suspend";
+      in
+      {
+        lidSwitch = defaultAction;
+        lidSwitchDocked = defaultAction;
+        lidSwitchExternalPower = defaultAction;
+        suspendKey = defaultAction;
+        suspendKeyLongPress = defaultAction;
+        rebootKey = defaultAction;
+        rebootKeyLongPress = defaultAction;
+        powerKey = defaultAction;
+        powerKeyLongPress = defaultAction;
+        hibernateKey = defaultAction;
+        hibernateKeyLongPress = defaultAction;
+        killUserProcesses = true;
+        extraConfig = customConfigs.LOGIND_CONFIG;
+      };
+  };
 
-  # services = {
-  #   avahi = {
-  #     enable = true;
-  #     nssmdns4 = true;
-  #   };
-  #   resolved = {
-  #     enable = true;
-  #     extraConfig = customConfigs.RESOLVED_CONFIG;
-  #   };
-  #   logind =
-  #     let
-  #       defaultAction = "lock";
-  #       suspendAction = "suspend";
-  #     in
-  #     {
-  #       lidSwitch = defaultAction;
-  #       lidSwitchDocked = defaultAction;
-  #       lidSwitchExternalPower = defaultAction;
-  #       suspendKey = defaultAction;
-  #       suspendKeyLongPress = defaultAction;
-  #       rebootKey = defaultAction;
-  #       rebootKeyLongPress = defaultAction;
-  #       powerKey = defaultAction;
-  #       powerKeyLongPress = defaultAction;
-  #       hibernateKey = defaultAction;
-  #       hibernateKeyLongPress = defaultAction;
-  #       killUserProcesses = true;
-  #       extraConfig = customConfigs.LOGIND_CONFIG;
-  #     };
-  # };
-
-  # programs = {
-  #   ssh.extraConfig = customConfigs.SSH_CLIENT_CONFIG;
-  #   gnupg.agent = {
-  #     enable = true;
-  #     pinentryPackage = pkgs.pinentry-curses;
-  #     enableSSHSupport = true;
-  #   };
-  #   git = {
-  #     enable = true;
-  #     config = {
-  #       init.defaultBranch = "main";
-  #       color.ui = "auto";
-  #       push.autoSetupRemote = true;
-  #       push.default = "current";
-  #       pull.rebase = true;
-  #       fetch.prune = true;
-  #       fetch.pruneTags = true;
-  #       alias.acommit = "commit --amend --no-edit --all";
-  #       alias.fpush = "push --force-with-lease";
-  #       rerere.enabled = true;
-  #     };
-  #   };
-  # };
+  programs = {
+    ssh.extraConfig = customConfigs.SSH_CLIENT_CONFIG;
+    gnupg.agent = {
+      enable = true;
+      pinentryPackage = pkgs.pinentry-curses;
+      enableSSHSupport = true;
+    };
+    git = {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+        color.ui = "auto";
+        push.autoSetupRemote = true;
+        push.default = "current";
+        pull.rebase = true;
+        fetch.prune = true;
+        fetch.pruneTags = true;
+        alias.acommit = "commit --amend --no-edit --all";
+        alias.fpush = "push --force-with-lease";
+        rerere.enabled = true;
+      };
+    };
+  };
 }
