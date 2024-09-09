92a99269e4307d53cadb766f24013af242741583
Armin <Armin.Mahdilou@gmail.com>
2024-09-09 11:56:14 +0200

    2024/09/09-11:56:14

--- system/rpi5.nix
+++ system/rpi5.nix
@@ -6,12 +6,12 @@
 }:
 with lib;
 {
-  boot = {
-    kernelParams = [ "dtb=\\bcm2712-rpi-5-b.dtb" ];
-    loader = {
-      efi.canTouchEfiVariables = false;
-    };
-  };
+  # boot = {
+  #   kernelParams = [ "dtb=\\bcm2712-rpi-5-b.dtb" ];
+  #   loader = {
+  #     efi.canTouchEfiVariables = false;
+  #   };
+  # };
   services = {
     connman = {
       enable = true;
