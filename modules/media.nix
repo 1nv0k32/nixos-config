153ff567519d9e8f31c5361a853fa426d8ab8734
Armin <Armin.Mahdilou@gmail.com>
2024-10-05 17:17:35 +0200

    2024/10/05-17:17:35

--- modules/media.nix
+++ modules/media.nix
@@ -3,22 +3,22 @@ let
   media_dir = "/data/media";
 in
 {
-  # users.users.minidlna = {
-  #   extraGroups = [ "users" ];
-  # };
+  users.users.minidlna = {
+    extraGroups = [ "users" ];
+  };
 
   systemd.tmpfiles.rules = [ "d ${media_dir} 0755 transmission users" ];
 
   services = {
-    # minidlna = {
-    #   enable = true;
-    #   openFirewall = true;
-    #   settings = {
-    #     friendly_name = "nyxpi DLNA MEDIA";
-    #     inotify = "yes";
-    #     media_dir = [ "V,${media_dir}" ];
-    #   };
-    # };
+    minidlna = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        friendly_name = "nyxpi DLNA MEDIA";
+        inotify = "yes";
+        media_dir = [ "V,${media_dir}" ];
+      };
+    };
 
     transmission = {
       enable = true;
