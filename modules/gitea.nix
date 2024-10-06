f119d1559934516e4b8deeb52da200f0be499e32
Armin <Armin.Mahdilou@gmail.com>
2024-10-07 00:30:25 +0200

    2024/10/07-00:30:25

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ config, ... }:
 {
   services.gitea = {
     enable = true;
     settings = {
       server.HTTP_ADDR = "0.0.0.0";
-      server.ROOT_URL = "http://nyxpi:22/";
+      server.ROOT_URL = "http://${config.networking.hostName}:22/";
       webhook.ALLOWED_HOST_LIST = "*";
     };
   };
