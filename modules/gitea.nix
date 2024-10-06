9baf9be2bae651430430bd609deb1fe5f7bb12bf
Armin <Armin.Mahdilou@gmail.com>
2024-10-07 00:08:09 +0200

    2024/10/07-00:08:09

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -3,10 +3,8 @@
   services.gitea = {
     enable = true;
     settings = {
-      server = {
-        HTTP_ADDR = "0.0.0.0";
-        ALLOWED_HOST_LIST = "*";
-      };
+      server.HTTP_ADDR = "0.0.0.0";
+      webhook.ALLOWED_HOST_LIST = "*";
     };
   };
 }
