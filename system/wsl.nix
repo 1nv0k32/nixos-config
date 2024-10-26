c9c8c751f54859326408ca38a7953cd5e93ee69c
Armin Mahdilou <Armin.Mahdilou@swisscom.com>
2024-10-26 17:45:29 +0200

    2024/10/26-17:45:29

--- system/wsl.nix
+++ system/wsl.nix
@@ -1,8 +1,13 @@
-{ config, ... }:
+{ lib, config, ... }:
 {
   wsl = {
     enable = true;
     startMenuLaunchers = true;
     defaultUser = config.environment.sysConf.mainUser;
   };
+
+  services.resolved.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.initrd.systemd.enable = lib.mkForce false;
+  boot.binfmt.emulatedSystems = lib.mkForce [ ];
 }
