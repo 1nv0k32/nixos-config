f1ab0ae624a6ddb8f4dec23b86a135ee258100c6
Armin Mahdilou <Armin.Mahdilou@swisscom.com>
2024-10-26 17:48:07 +0200

    2024/10/26-17:48:07

--- system/wsl.nix
+++ system/wsl.nix
@@ -6,6 +6,11 @@
     defaultUser = config.environment.sysConf.mainUser;
   };
 
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
+
   services.resolved.enable = lib.mkForce false;
   boot = {
     loader.systemd-boot.enable = lib.mkForce false;
