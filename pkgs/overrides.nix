4f8cdd58db32125750fc1589688312442a7fcb0b
Armin Mahdilou <Armin.Mahdilou@swisscom.com>
2024-10-27 02:14:30 +0200

    2024/10/27-02:14:30

--- pkgs/overrides.nix
+++ pkgs/overrides.nix
@@ -1,8 +1,12 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (vagrant.overrideAttrs (oldAttrs: rec {
-      installCheckPhase = '''';
+    (vagrant.overrideAttrs (oldAttrs: {
+      installCheckPhase =
+        ''
+          export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="0"
+        ''
+        + oldAttrs.installCheckPhase;
     }))
   ];
 }
