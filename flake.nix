57ac8cd3567011498c3b2d39e357edd56f215bac
Armin <Armin.Mahdilou@gmail.com>
2024-11-03 21:53:07 +0100

    Revert "2024/11/03-21:51:28"
    This reverts commit f7b7a094b7e12d480f12360362d48704e0189ddc.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
@@ -23,7 +23,7 @@
   outputs =
     { self, ... }@inputs:
     {
-      stateVersion = "24.11";
+      stateVersion = "24.05";
       baseModules = [
         inputs.home-manager.nixosModules.home-manager
         inputs.nixvim.nixosModules.nixvim
