31174b6cba1c34657be4bbf21abd348474a80683
Armin <Armin.Mahdilou@gmail.com>
2024-11-04 22:00:23 +0100

    2024/11/04-22:00:23

--- flake.nix
+++ flake.nix
@@ -101,7 +101,8 @@
             hostName = prop.hostName;
           };
           modules = [
-            inputs.home-manager.nixosModules.home-manager
+            inputs.nixvim.nixosModules.nixvim
+            (import "${self}/pkgs/overlays.nix" { inherit inputs; })
             (import "${self}/modules")
             (import "${self}/src")
             (import "${self}/system/droid.nix")
