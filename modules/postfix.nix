56f7ab7c77105c185738d1c1efe7147176681aef
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-02-15 23:42:02 +0100

    2025/02/15-23:42:02

--- modules/postfix.nix
+++ modules/postfix.nix
@@ -5,9 +5,9 @@
     relayHost = "smtp.gmail.com";
     relayPort = 587;
     config = {
-      smtp_use_tls = "yes";
       smtp_sasl_auth_enable = "yes";
-      smtp_sasl_security_options = "";
+      smtp_sasl_security_options = "noanonymous";
+      smtp_tls_security_level = "encrypt";
       smtp_sasl_password_maps = "texthash:${config.environment.sysConf.postfixSaslPasswordPath}";
     };
   };
