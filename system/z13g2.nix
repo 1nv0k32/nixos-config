e2d69fc0ae8d74166fce81c4520290205496b9a7
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-02-18 16:17:47 +0100

    2025/02/18-16:17:47

--- system/z13g2.nix
+++ system/z13g2.nix
@@ -1,4 +1,9 @@
-{ lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   boot = {
     extraModprobeConfig = ''
@@ -13,7 +18,6 @@
   };
 
   services = {
-    fprintd.enable = lib.mkForce false;
     power-profiles-daemon.enable = lib.mkForce false;
     tlp = {
       enable = true;
@@ -23,5 +27,28 @@
         CPU_BOOST_ON_BAT = 0;
       };
     };
+  };
+
+  # fingerprint
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
   };
 }
