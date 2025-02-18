de77b6b809920d6a1254d58dae9145032fc589d4
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-02-18 16:20:04 +0100

    2025/02/18-16:20:04

--- system/z13g2.nix
+++ system/z13g2.nix
@@ -42,11 +42,8 @@
       auth       required                    pam_env.so
       auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
       auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
-
       account    include                     login
-
       password   required                    pam_deny.so
-
       session    include                     login
       session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
     '';
