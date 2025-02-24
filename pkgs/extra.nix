7b9a88f6347726291f7099716498ebde4c497da9
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-02-24 19:12:23 +0100

    2025/02/24-19:12:23

--- pkgs/extra.nix
+++ pkgs/extra.nix
@@ -65,8 +65,8 @@
     libreoffice
 
     otpclient
-    pkgs-master.yubikey-manager
-    pkgs-master.yubikey-manager-qt
+    yubikey-manager
+    yubikey-manager-qt
 
     win-virtio
     virt-manager
@@ -89,9 +89,9 @@
   ];
 
   services.udev.packages = with pkgs; [
-    pkgs-master.yubikey-personalization
     platformio-core
     openocd
+    yubikey-personalization
     proxmark3
   ];
 }
