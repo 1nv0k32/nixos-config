54e70b8316f2f59f478545f3ec5cb1291a9bcb1f
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-03-07 18:00:43 +0100

    2025/03/07-18:00:43

--- system/hetzner.nix
+++ system/hetzner.nix
@@ -1,32 +1,32 @@
 { lib, ... }:
 {
-  disko.devices.disk.sda = {
-    type = "disk";
-    device = "/dev/sda";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          type = "EF00";
-          size = "256M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        };
-      };
-    };
-  };
+  # disko.devices.disk.sda = {
+  #   type = "disk";
+  #   device = "/dev/sda";
+  #   content = {
+  #     type = "gpt";
+  #     partitions = {
+  #       ESP = {
+  #         type = "EF00";
+  #         size = "256M";
+  #         content = {
+  #           type = "filesystem";
+  #           format = "vfat";
+  #           mountpoint = "/boot";
+  #           mountOptions = [ "umask=0077" ];
+  #         };
+  #       };
+  #       root = {
+  #         size = "100%";
+  #         content = {
+  #           type = "filesystem";
+  #           format = "ext4";
+  #           mountpoint = "/";
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
   boot = {
     loader = {
