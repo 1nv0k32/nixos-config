db04c4164a4d95f323227a4b9fa384bb9cce90fe
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-03-08 01:41:21 +0100

    2025/03/08-01:41:21

--- system/hetzner.nix
+++ system/hetzner.nix
@@ -25,16 +25,23 @@ in
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJ414vPwc8EjJPtKSGTSUCWuSQNifNI3pv+/sodmkAd8Ww6ofCxPcrJ1UjgTXis2Oqk2I1EGtRnD2ozom+jW+AZ5R00Ht2+1RGD+dkd0rrIqi+HFEVFPUDCu1pAZnLOzfGrFGpHpWkrbDqtv7OFA60g9iIAaId1Fi1VOSR43s4XYmTmWio7R090LW6aENb+jc2vW7VSKq/MlgTtvfKEHm187RFNHf+2BGp8Cdq6rwSEbzkkiQ9kI0W6JxCGRRDf0qIoAu6TVpjoHlzXAEFUZaz4EtxM2EmyhiRop4vVougUqtnRTzq8+RRMoVECXOylJGWbd8MX25MnRbCGph7UXuZ8sGrvwsJBjbDhWRuIgYfJdnf+ymuki361F8qIvBC7xHsBGVfEpVXcGuSC9GaBw8Y3XLMBtpcUXfuT1Tz1O/U+QwC5mdAgGwZ2BQkBfP8edtpeg5LkSgcU7F4LPaXAEnzievqljyS4UUV0vqF/2iTM72q8wlM5XO7GsWkY7/B6g/Ha3080yypUza9lG20CAaeCso4CsjehF3qd4S4BcZeXUpaOUYHYk6f2M/QEReAY1viJAsx4o6dQ2sArg8JM/5IWBRWzM/NK+RrPvCWzk8lmsMBEaKkPWEpBe7H07qbOn2fDDBQxL0ImoGdXtqLNH7lOZGYZVKbZZ5ZknZB4EeIKQ== rick@nyx"
   ];
 
-  systemd.network.networks."30-wan" = {
-    matchConfig.Name = "enp1s0";
-    networkConfig.DHCP = "no";
-    address = [ ];
-    routes = [
+  networking.interfaces.enp1s0 = {
+    useDHCP = "false";
+    ipv4.addresses = [ ];
+    ipv4.routes = [
       {
-        Gateway = "172.31.1.1";
-        GatewayOnLink = true;
+        address = "0.0.0.0";
+        prefixLength = "0";
+        via = "172.31.1.1";
       }
-      { Gateway = "fe80::1"; }
+    ];
+    ipv6.addresses = [ ];
+    ipv6.routes = [
+      {
+        address = "::";
+        prefixLength = "0";
+        via = "fe80::1";
+      }
     ];
   };
 
