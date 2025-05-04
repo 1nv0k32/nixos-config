8d5f45b310d74980ce9734e33ee564e5e85eb644
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-05-04 11:54:23 +0200

    2025/05/04-11:54:23

--- system/z13g2.nix
+++ system/z13g2.nix
@@ -28,7 +28,6 @@
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        CPU_SCALING_MAX_FREQ_ON_BAT = 1200000;
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
       };
