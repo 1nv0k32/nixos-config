d1397faf82e0bf5dde683a4eae75efb047623f61
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-05-25 00:40:34 +0200

    2025/05/25-00:40:34

--- system/vm.nix
+++ system/vm.nix
@@ -1,14 +1,6 @@
+{ ... }:
 {
-  modulesPath,
-  ...
-}:
-{
-  # imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
-  services.qemuGuest.enable = true;
-
   virtualisation = {
-    # cores = 4;
-    # memorySize = 8 * 1024;
     diskSize = 200 * 1024;
   };
 }
