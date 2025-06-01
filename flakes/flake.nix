fe3be39220139e7badd4fd5946451470ba916c50
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-02 01:29:43 +0200

    2025/06/02-01:29:43

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -7,48 +7,51 @@
 
   outputs =
     { cfg, ... }:
-    let
-      mkHost = host: systemType: systemType {
-        hostName = host;
-        modules = cfg.optionalLocalModules [
-          ./hardware-configuration.nix
-          ./local.nix
-        ];
-      };
-    in
     {
-      nixosConfigurations = {
-        mapAttrs mkHost {
+      nixosConfigurations =
+        let
+          mkHost =
+            host: systemType:
+            systemType {
+              hostName = host;
+              modules = cfg.optionalLocalModules [
+                ./hardware-configuration.nix
+                ./local.nix
+              ];
+            };
+        in
+        builtins.mapAttrs mkHost {
           nyx = cfg.systemTypes.z13g2;
         };
-        nyxvm = cfg.systemTypes.vm {
-          hostName = "nyxvm";
-          modules = cfg.optionalLocalModules [
-            ./hardware-configuration.nix
-            ./local.nix
-          ];
-        };
-        nyxwsl = cfg.systemTypes.wsl {
-          hostName = "arminix";
-          modules = cfg.optionalLocalModules [
-            ./hardware-configuration.nix
-            ./local.nix
-          ];
-        };
-        nyxpi = cfg.systemTypes.rpi5 {
-          hostName = "nyxpi";
-          modules = cfg.optionalLocalModules [
-            ./hardware-configuration.nix
-            ./local.nix
-          ];
-        };
-        hub = cfg.systemTypes.hetzner {
-          hostName = "hub";
-          modules = cfg.optionalLocalModules [
-            ./hardware-configuration.nix
-            ./local.nix
-          ];
-        };
-      };
+      #     nyxvm = cfg.systemTypes.vm {
+      #       hostName = "nyxvm";
+      #       modules = cfg.optionalLocalModules [
+      #         ./hardware-configuration.nix
+      #         ./local.nix
+      #       ];
+      #     };
+      #     nyxwsl = cfg.systemTypes.wsl {
+      #       hostName = "arminix";
+      #       modules = cfg.optionalLocalModules [
+      #         ./hardware-configuration.nix
+      #         ./local.nix
+      #       ];
+      #     };
+      #     nyxpi = cfg.systemTypes.rpi5 {
+      #       hostName = "nyxpi";
+      #       modules = cfg.optionalLocalModules [
+      #         ./hardware-configuration.nix
+      #         ./local.nix
+      #       ];
+      #     };
+      #     hub = cfg.systemTypes.hetzner {
+      #       hostName = "hub";
+      #       modules = cfg.optionalLocalModules [
+      #         ./hardware-configuration.nix
+      #         ./local.nix
+      #       ];
+      #     };
+      #   };
+      # };
     };
 }
