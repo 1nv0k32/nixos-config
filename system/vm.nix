3592bd4c5fe9aa186146e707a51fe0962477e55d
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-02 01:48:50 +0200

    2025/06/02-01:48:50

--- system/vm.nix
+++ system/vm.nix
@@ -3,7 +3,6 @@
   imports = [ (modulesPath + "/virtualisation/qemu-vm.nix") ];
 
   virtualisation = {
-    useBootLoader = true;
     cores = 8;
     memorySize = 16 * 1024;
     diskSize = 200 * 1024;
