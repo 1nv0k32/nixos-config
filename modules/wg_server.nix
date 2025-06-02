75fe681d009400f70bc6e79ce2e4effd0c9ecd83
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-02 19:18:35 +0200

    2025/06/02-19:18:35

--- modules/wg_server.nix
+++ modules/wg_server.nix
@@ -1,22 +1,32 @@
 { lib, config, ... }:
 let
-  cfg = config.environment.sysConf.wg;
+  cfg = config.environment.sysConf.server.wg;
 in
 {
-  options.environment.sysConf.wg = {
-    interface = lib.mkOption {
-      type = lib.types.str;
-      default = "wg0";
-    };
+  options.environment.sysConf = {
+    server.wg = {
+      interface = lib.mkOption {
+        type = lib.types.str;
+        default = "wg0";
+      };
 
-    ip = lib.mkOption {
-      type = lib.types.str;
-      default = 51820;
-    };
+      ip = lib.mkOption {
+        type = lib.types.str;
+        default = "10.100.0.1/24";
+      };
 
-    port = lib.mkOption {
-      type = lib.types.str;
-      default = 51820;
+      port = lib.mkOption {
+        type = lib.types.int;
+        default = 51820;
+      };
+
+      privateKeyFile = lib.mkOption {
+        type = lib.types.str;
+      };
+
+      peers = lib.mkOption {
+        type = lib.types.listOf lib.types.attrs;
+      };
     };
   };
 
@@ -26,25 +36,20 @@ in
       enable = true;
       netdevs."50-${cfg.interface}" = {
         netdevConfig = {
-          Kind = "wireguard";
           Name = cfg.interface;
+          Kind = "wireguard";
           MTUBytes = "1300";
         };
         wireguardConfig = {
-          PrivateKeyFile = "/run/keys/wireguard-privkey";
           ListenPort = cfg.port;
+          PrivateKeyFile = cfg.privateKeyFile;
           RouteTable = "main";
         };
-        wireguardPeers = [
-          {
-            PublicKey = "L4msD0mEG2ctKDtaMJW2y3cs1fT2LBRVV7iVlWZ2nZc=";
-            AllowedIPs = [ "10.100.0.2" ];
-          }
-        ];
+        wireguardPeers = cfg.peers;
       };
       networks.${cfg.interface} = {
         matchConfig.Name = cfg.interface;
-        address = [ "10.100.0.1/24" ];
+        address = [ cfg.ip ];
         networkConfig = {
           IPMasquerade = "ipv4";
           IPForward = true;
