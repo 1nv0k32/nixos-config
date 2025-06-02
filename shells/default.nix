77074972b89997b03a222ed6e0e1119ebb71a9f3
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-02 03:29:46 +0200

    2025/06/02-03:29:46

--- shells/default.nix
+++ shells/default.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
-  default = pkgs.mkShell {
-    shellHook = ''
-      nixconf() (
+  default =
+    let
+      nixconf = pkgs.writeShellScriptBin "nixconf" ''
+        #! ${pkgs.stdenv.shell}
         set -euo pipefail
         [ -f flake.nix ] && [ -f flake.lock ] || false
         cd $(${pkgs.git}/bin/git rev-parse --show-toplevel 2> /dev/null)
@@ -26,30 +27,18 @@
               ;;
           esac
         done
-      )
-
-      nixup() (
+      '';
+      nixup = pkgs.writeShellScriptBin "nixconf" ''
+        #! ${pkgs.stdenv.shell}
+        set -euo pipefail
         sudo nix flake update --flake path:/etc/nixos
         sudo nixos-rebuild switch
-      )
-
-      # Export for direnv
-      export_function() {
-        local name=$1
-        local alias_dir=$PWD/.direnv/aliases
-        mkdir -p "$alias_dir"
-        PATH_add "$alias_dir"
-        local target="$alias_dir/$name"
-        if declare -f "$name" >/dev/null; then
-            echo "#!/usr/bin/env bash" > "$target"
-            declare -f "$name" >> "$target" 2>/dev/null
-            echo "$name" >> "$target"
-            chmod +x "$target"
-        fi
-      }
-
-      export_function nixconf
-      export_function nixup
-    '';
-  };
+      '';
+    in
+    pkgs.mkShell {
+      buildInputs = [
+        nixconf
+        nixup
+      ];
+    };
 }
