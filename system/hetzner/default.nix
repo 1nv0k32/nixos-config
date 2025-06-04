dbe6e8fe7fb020f4b81a63f1622d6e578e39ddde
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-04 11:49:04 +0200

    2025/06/04-11:49:04

--- system/hetzner/default.nix
+++ system/hetzner/default.nix
@@ -5,6 +5,10 @@
     (import ../../modules/wg_server.nix)
   ];
 
+  environment.etc."nixos/flake.nix" = {
+    source = ../../flakes/flake.nix;
+  };
+
   networking = {
     useNetworkd = true;
     useDHCP = false;
