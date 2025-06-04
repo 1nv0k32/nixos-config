89aa6769e23723b86bd0b1e1a92c19bee7e51d59
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-04 11:22:03 +0200

    2025/06/04-11:22:03

--- system/hetzner/default.nix
+++ system/hetzner/default.nix
@@ -1,11 +1,10 @@
-{ lib, ... }:
+{ ... }:
 {
   imports = [
     (import ./disko.nix)
     (import ../../modules/wg_server.nix)
   ];
 
-  boot.initrd.systemd.enable = lib.mkForce false;
   networking = {
     useNetworkd = true;
     useDHCP = false;
