e560fa233ebd9744f6f669c4d4f6e169d071e209
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-16 17:03:21 +0200

    2025/06/16-17:03:21

--- system/containers/default.nix
+++ system/containers/default.nix
@@ -1,10 +1,10 @@
-{ ... }@attrs:
+{ ... }:
 {
   containers = {
     server01 = {
       autoStart = true;
       privateNetwork = true;
-      config = (import ./server01.nix attrs);
+      config = (import ./server01.nix);
     };
   };
 }
