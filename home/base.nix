851ff73b833df0058fde617c54e33580a8b3918e
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-24 15:10:25 +0200

    2025/06/24-15:10:25

--- home/base.nix
+++ home/base.nix
@@ -35,6 +35,14 @@ in
     enable = true;
     enableBashIntegration = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        load_dotenv = true;
+        strict_env = true;
+        hide_env_diff = true;
+        warn_timeout = 0;
+      };
+    };
   };
 
   programs.readline = {
