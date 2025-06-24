990bfb33374ef6b25841539b3d9c573643c479dc
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-24 15:15:32 +0200

    2025/06/24-15:15:32

--- home/base.nix
+++ home/base.nix
@@ -31,20 +31,6 @@ in
     bashrcExtra = customConfigs.DOT_BASHRC;
   };
 
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    nix-direnv.enable = true;
-    config = {
-      global = {
-        load_dotenv = true;
-        strict_env = true;
-        hide_env_diff = true;
-        warn_timeout = 0;
-      };
-    };
-  };
-
   programs.readline = {
     enable = true;
     extraConfig = ''
---
+++ modules/direnv.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    loadInNixShell = true;
+    silent = true;
+    nix-direnv.enable = true;
+    settings = {
+      global = {
+        load_dotenv = true;
+        strict_env = true;
+        warn_timeout = 0;
+      };
+    };
+  };
+}
