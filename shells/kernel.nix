e8b660a79ec89b7ce06dd2cae0d2e340551f2d87
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-24 13:46:35 +0200

    2025/06/24-13:46:35

--- shells/kernel.nix
+++ shells/kernel.nix
@@ -1,5 +1,50 @@
 { pkgs, ... }:
 let
+  bashOpts = ''
+    set -euo pipefail
+  '';
+  krun = pkgs.writeShellScriptBin "k.run" ''
+    ${bashOpts}
+    qemu-system-x86_64 \
+    -kernel linux/arch/x86/boot/bzImage \
+    -initrd initramfs.img \
+    -enable-kvm -machine pc-q35-7.1 \
+    -m 2G -smp 1,sockets=1,cores=1 \
+    -net nic,model=e1000 \
+    -nographic -serial mon:stdio \
+    -s -append 'console=ttyS0 earlyprintk=serial net.ifnames=0 nokaslr'
+  '';
+  kinit = pkgs.writeShellScriptBin "k.init" ''
+    ${bashOpts}
+    INITRAMFS_DIR=initramfs
+    BUSYBOX_DIR=busybox
+    LINUX_DIR=linux
+
+    cat <<-EOF > $INITRAMFS_DIR/init
+      #!/bin/sh
+      mkdir -p /dev
+      mkdir -p /proc
+      mkdir -p /sys
+      mount -t devtmpfs devtmpfs /dev
+      mount -t proc none /proc
+      mount -t sysfs none /sys
+      exec env HOME=/root /bin/sh
+    EOF
+    chmod +x $INITRAMFS_DIR/init
+    (
+      cd $BUSYBOX_DIR
+      make
+      make CONFIG_PREFIX=../$INITRAMFS_DIR install
+    )
+    (
+      cd $LINUX_DIR
+      make INSTALL_MOD_PATH=../$INITRAMFS_DIR modules_install
+    )
+    (
+      cd $INITRAMFS_DIR
+      find . -print | cpio -H newc -o > ../initramfs.img
+    )
+  '';
   defaultDevShell = prop: {
     nativeBuildInputs =
       with pkgs;
@@ -22,6 +67,9 @@ let
         gdb
         autoconf
         automake
+        # scripts
+        kinit
+        krun
       ]
       ++ (with pkgs.llvmPackages; [
         llvm
