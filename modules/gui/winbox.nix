c40434b8820df3e2d082f08ad7ce64db8898e648
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-06-25 10:12:52 +0200

    2025/06/25-10:12:52

--- modules/gui/winbox.nix
+++ modules/gui/winbox.nix
@@ -1,15 +1,12 @@
-{ pkgs, self, ... }:
+{ pkgs, ... }:
 {
-  imports = [
-    (import "${self.inputs.nixpkgs-test}/nixos/modules/programs/winbox.nix")
-  ];
   # networking.firewall.allowedUDPPortRanges = [
   #   {
   #     from = 40000;
   #     to = 50000;
   #   }
   # ];
-  programs.winboxx = {
+  programs.winbox = {
     enable = true;
     openFirewall = true;
     package = pkgs.pkgs-unstable.winbox;
