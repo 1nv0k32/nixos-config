cbc70e0790d19bb04abdcbb154fc9dc012401ea5
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-02 21:42:26 +0200

    2025/07/02-21:42:26

--- system/utm/default.nix
+++ system/utm/default.nix
@@ -7,14 +7,8 @@
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
-    loader.grub.devices = lib.mkDefault [ "/dev/vda" ];
     initrd.kernelModules = [ "virtio_gpu" ];
     growPartition = true;
-  };
-
-  fileSystems."/" = lib.mkDefault {
-    device = "/dev/vda1";
-    fsType = "ext4";
   };
 
   networking.useNetworkd = true;
