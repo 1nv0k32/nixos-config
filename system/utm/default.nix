f227c2b5c9a40a52aee9682068bbab06ecfbaf91
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-02 21:34:15 +0200

    2025/07/02-21:34:15

--- system/utm/default.nix
+++ system/utm/default.nix
@@ -1,6 +1,24 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     (import ./disko.nix)
   ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.grub.devices = lib.mkDefault [ "/dev/vda" ];
+    initrd.kernelModules = [ "virtio_gpu" ];
+    growPartition = true;
+  };
+
+  fileSystems."/" = lib.mkDefault {
+    device = "/dev/vda1";
+    fsType = "ext4";
+  };
+
+  networking.useNetworkd = true;
+  networking.useDHCP = false;
+
+  services.qemuGuest.enable = lib.mkDefault true;
 }
