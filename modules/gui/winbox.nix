09db1aca4b8ddd1a909bec9f91c1d98620684070
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-04 23:32:03 +0200

    2025/07/04-23:32:03

--- modules/gui/winbox.nix
+++ modules/gui/winbox.nix
@@ -6,11 +6,12 @@
   ...
 }:
 {
+  disabledModules = [ "programs/winbox.nix" ];
+  imports = [
+    (import "${self.inputs.nixpkgs-master}/nixos/modules/programs/winbox.nix")
+  ];
+
   config = lib.mkIf config.environment.sysConf.x86 {
-    disabledModules = [ "programs/winbox.nix" ];
-    imports = [
-      (import "${self.inputs.nixpkgs-master}/nixos/modules/programs/winbox.nix")
-    ];
     programs.winbox = {
       enable = true;
       openFirewall = true;
