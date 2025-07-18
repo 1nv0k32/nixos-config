f71429b72664e7d4d3ea003f42b8968c34146c1d
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-18 22:15:01 +0200

    2025/07/18-22:15:01

--- src/options.nix
+++ src/options.nix
@@ -15,8 +15,8 @@
       sshPubKeys = lib.mkOption {
         type = lib.types.listOf lib.types.singleLineStr;
         default = [
-          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILGqq4qABf8hNbaIr2eiKl5GAaLQ0WSVmNlDG+4iW3zRAAAABHNzaDo= rick@nyx"
-          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPX+my/+qVA9NN/TrX0EU3SLsP2tVgYIh6wSV2GAKQ3iAAAABHNzaDo= rick@nyx"
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILGqq4qABf8hNbaIr2eiKl5GAaLQ0WSVmNlDG+4iW3zRAAAABHNzaDo= rick@nyx" # 5c
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFxZAf+KK+2G9v+YnhQpex15Xd2dKDkFF4rNpKyIz63NAAAABHNzaDo= rick@nyx" # bio
         ];
         description = "The main user's trusted SSH key";
       };
@@ -24,11 +24,11 @@
         type = lib.types.str;
         default =
           let
-            key01 = "WE4Y7oGkwsG3MsWOfEA9fRsokjRpHZnPkP046Fx4HJA1UVZgWGGbEUgBu3elHZB78oSLtW+MYbNzJ4OOQw74RQ==,ymae5yWDDe5H0tI5IRi2R7cjnPaBGad+cwe7LXvQbj9VKJdF9f5+Cno4yYiRomBVDxurkN9b/Lc/KZ6oJt9Y5A==,es256,";
-            key02 = "Uuvn7svQcdXRQqQ2wCc8RRMAYt5p8huHUTlbEgBzYOVjPnxVxL8JwZaD9GVT0kYWo0k74nYlKIIFyILs4KKOuA==,9vvs3rm0NVl6xq5Ql9Y6TSuYGG69QAHeJCURHLh6fmnEg6aKkZLTNzrBdMwvNMVQF6ij5hjXJGepebMda+/q2Q==,es256,";
+            key_5c = "qJzOdjN/bRt9nqlPPpT3ULxN0omg9EgvhpiIc5/xeqY5dQrk+K2Sc6IGYqIXne3oWV9usY/pq6gxfTom42DtGg==,+HdbjR3Pp5SAhRiDFSkIiUWCtpTHv81c9fV7aNcnUdaiKVWMQbFsgpqsPsG0MG3dx76bCjB1QhOKxv6k7V4+QQ==,es256,";
+            key_bio = "7wwRJCaiRcL/HwR8JK7hdITpvcsX55jKDtY7ckBhUB00p8Id2s5AykzVPq79dbKvkiQhhcrQvd3bT1w3x16Nyw==,ijaKeTEL3VG81mm+0zi0XxZe8uXbcR8j8Rr/oZZGn3FbpTlWm+5wRyYjx0hPz02Ve8QyspXg935VnHl8lRMahw==,es256,+presence";
           in
           ''
-            ${key01}:${key02}
+            ${key_5c}:${key_bio}
           '';
       };
     };
