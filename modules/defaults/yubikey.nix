bf1af2fb05cd09352e76015b64848923e71f4c71
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-21 23:05:30 +0200

    2025/07/21-23:05:30

--- modules/defaults/yubikey.nix
+++ modules/defaults/yubikey.nix
@@ -23,6 +23,14 @@ in
   services = {
     pcscd.enable = true;
     yubikey-agent.enable = true;
+    udev.extraRules = ''
+      ACTION=="remove",\
+      ENV{ID_BUS}=="usb",\
+      ENV{ID_MODEL_ID}=="0407",\
+      ENV{ID_VENDOR_ID}=="1050",\
+      ENV{ID_VENDOR}=="Yubico",\
+      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+    '';
   };
 
   security = {
