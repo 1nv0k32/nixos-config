3973fbc66a9f3d7ad52aec4d2650e41c18500e6f
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-07-24 23:41:22 +0200

    2025/07/24-23:41:22

--- flake.nix
+++ flake.nix
@@ -224,10 +224,10 @@
           goShells = (import "${self}/shells/go.nix" { inherit pkgs; });
         in
         {
-          ${system}.default = defaultShells.shell;
-          ${system}.kernel = kernelShells.shell;
-          ${system}.python = pythonShells.shell;
-          ${system}.go = goShells.shell;
+          default = defaultShells.shell;
+          kernel = kernelShells.shell;
+          python = pythonShells.shell;
+          go = goShells.shell;
         }
       );
     };
