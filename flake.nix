4bb5aea7ac8ee827a39dc4fe0f9eac833234f731
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-08-19 00:09:50 +0200

    2025/08/19-00:09:50

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
           nixpkgs.lib.nixosSystem {
             system = flake-utils.lib.system.x86_64-linux;
             specialArgs = {
-              inherit self;
+              inherit self system;
               inherit (attrs) hostName;
             };
             modules = [
