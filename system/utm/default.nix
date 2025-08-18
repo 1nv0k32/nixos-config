8e9ae21cdaa0fb04bb4be6d26d3781bcaacc798e
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-08-18 23:11:31 +0200

    2025/08/18-23:11:31

--- system/utm/default.nix
+++ system/utm/default.nix
@@ -1,23 +1,8 @@
-{ modulesPath, ... }:
+{ self, ... }:
 {
   imports = [
-    "${modulesPath}/profiles/qemu-guest.nix"
+    (import "${self}/system/vm.nix")
     (import ./disko.nix)
   ];
-
-  boot = {
-    growPartition = true;
-    initrd.kernelModules = [
-      "virtio_pci"
-      "xhci_pci"
-      "usb_storage"
-      "usbhid"
-    ];
-  };
-
   virtualisation.rosetta.enable = true;
-
-  services = {
-    spice-vdagentd.enable = true;
-  };
 }
--- system/vm.nix
+++ system/vm.nix
@@ -1,4 +1,20 @@
-{ ... }:
+{ modulesPath, ... }:
 {
+  imports = [
+    "${modulesPath}/profiles/qemu-guest.nix"
+  ];
 
+  boot = {
+    growPartition = true;
+    initrd.kernelModules = [
+      "virtio_pci"
+      "xhci_pci"
+      "usb_storage"
+      "usbhid"
+    ];
+  };
+
+  services = {
+    spice-vdagentd.enable = true;
+  };
 }
