faa0a8c91a5f28c20362f6fec1ff5638b6bc4bac
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-08-21 20:52:23 +0200

    2025/08/21-20:52:23

--- system/containers/os-master.nix
+++ system/containers/os-master.nix
@@ -4,6 +4,10 @@
     openstack-nix.nixosModules."x86_64-linux".controllerModule
   ];
 
+  networking.extraHosts = ''
+    127.0.0.1 controller controller.local
+  '';
+
   system = {
     stateVersion = self.nixosModules.stateVersion;
   };
