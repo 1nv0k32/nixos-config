f455e7cc5d3bbae4b01a7e72b5a854db35c98482
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-08-22 00:02:27 +0200

    2025/08/22-00:02:27

--- system/containers/default.nix
+++ system/containers/default.nix
@@ -4,12 +4,16 @@
     controller = {
       autoStart = true;
       privateNetwork = true;
+      hostAddress = "10.0.0.1";
+      localAddress = "10.0.0.10";
       config = (import ./controller.nix attrs);
     };
 
     compute = {
       autoStart = true;
       privateNetwork = true;
+      hostAddress = "10.0.0.2";
+      localAddress = "10.0.0.11";
       config = (import ./compute.nix attrs);
     };
   };
