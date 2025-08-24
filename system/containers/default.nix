bdee9cdeec6720faa8bdff7445af5ede0a98f3f9
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-08-24 03:03:12 +0200

    2025/08/24-03:03:12

--- system/containers/default.nix
+++ system/containers/default.nix
@@ -1,15 +1,30 @@
 { pkgs, lib, ... }@attrs:
 {
+  networking = {
+    bridges.os1.interfaces = [ ];
+    interfaces.os1 = {
+      ipv4.addresses = [
+        {
+          address = "10.0.1.1";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
+
   containers = {
     controller = {
       autoStart = true;
       privateNetwork = true;
+      hostBridge = "os1";
+      localAddress = "10.0.1.100/24";
       config = (import ./controller.nix attrs);
     };
 
     compute = {
       autoStart = true;
       privateNetwork = true;
+      localAddress = "10.0.1.101/24";
       config = (import ./compute.nix attrs);
     };
   };
