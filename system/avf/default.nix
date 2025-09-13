9b19f8c02e0f9fe0b0f8fec9291543904c8f46e6
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-09-13 13:15:11 +0200

    2025/09/13-13:15:11

--- system/avf/default.nix
+++ system/avf/default.nix
@@ -3,6 +3,10 @@ let
   cfg = config.environment.sysConf;
 in
 {
+  users.users."${cfg.user.name}" = {
+    initialPassword = null;
+  };
+
   avf = {
     defaultUser = cfg.user.name;
     vmConfig = {
