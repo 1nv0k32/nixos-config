5fbd3895871260c47cfdcbe95993354dc5c0092f
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-10-04 02:24:11 +0200

    2025/10/04-02:24:11

--- system/z13g2/default.nix
+++ system/z13g2/default.nix
@@ -33,7 +33,7 @@
         STOP_CHARGE_THRESH_BAT0 = 100;
         CPU_DRIVER_OPMODE_ON_AC = "active";
         CPU_DRIVER_OPMODE_ON_BAT = "active";
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
         CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
