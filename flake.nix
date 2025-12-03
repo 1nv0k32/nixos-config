45aea8927201630204cd9b6238fa30d62db8524c
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-12-03 11:39:54 +0100

    2025/12/03-11:39:54

--- flake.nix
+++ flake.nix
@@ -3,16 +3,16 @@
     # nixpkgs
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-test.url = "github:1nv0k32/nixpkgs";
     # tools
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.05";
+      url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
