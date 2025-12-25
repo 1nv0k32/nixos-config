c4d35eb4d1584d77c8fa5a710e499fbdc95f6aca
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2025-12-25 15:28:44 +0100

    2025/12/25-15:28:44

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -9,7 +9,7 @@
     opts = {
       number = true;
       relativenumber = false;
-      guicursor = "";
+      guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20";
       undofile = true;
       encoding = "utf-8";
       signcolumn = "yes";
