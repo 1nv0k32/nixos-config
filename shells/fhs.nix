8a59cf237652d2cfcb8ab6d61ecc06e677130ba6
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-01-01 18:24:58 +0100

    2026/01/01-18:24:58

--- shells/fhs.nix
+++ shells/fhs.nix
@@ -1,34 +1,33 @@
 { pkgs, ... }:
-# let
-#   pythonWithPIO =
-#     pks:
-#     pks.python312.withPackages (
-#       ps: with ps; [
-#         pks.platformio
-#         pylibftdi
-#         pyusb
-#       ]
-#     );
-# in
-# {
-#   shell = pkgs.buildFHSEnv {
-#     name = "platformio-fhs";
-#     targetPkgs =
-#       pkgs:
-#       (with pkgs; [
-#         zsh
-#         platformio-core
-#         openocd
-#         arduino-cli
-#         avrdude
-#         libftdi
-#         libftdi1
-#         libusb1
-#         (pythonWithPIO pkgs)
-#       ]);
-#   };
+let
+  pythonWithPIO =
+    pks:
+    pks.python312.withPackages (
+      ps: with ps; [
+        platformio-core
+        pylibftdi
+        pyusb
+      ]
+    );
+in
 {
-  shell = pkgs.platformio // {
-    runScript = "${pkgs.zsh}/bin/zsh";
+  shell = pkgs.buildFHSEnv {
+    name = "platformio-fhs";
+    targetPkgs =
+      pkgs:
+      (with pkgs; [
+        zsh
+        platformio-core
+        openocd
+        arduino-cli
+        avrdude
+        libftdi
+        libftdi1
+        libusb1
+        (pythonWithPIO pkgs)
+      ]);
+    runScript = ''
+      ${pkgs.zsh}/bin/zsh
+    '';
   };
 }
