f0d4dae343f03985e57718ee9bd0e80bc9588752
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-01-02 19:08:42 +0100

    2026/01/02-19:08:42

--- shells/fhs.nix
+++ shells/fhs.nix
@@ -2,32 +2,34 @@
 {
   shell = pkgs.buildFHSEnv {
     name = "fhs";
-    targetPkgs = with pkgs; [
-      # # python
-      # uv
-      # python312
-      # # go
-      # libcap
-      # go
-      # gcc
-      # delve
-      # platformio
-      platformio-core
-      openocd
-      arduino-cli
-      avrdude
-      libftdi
-      libftdi1
-      libusb1
-    ];
+    targetPkgs =
+      pkgs:
+      (with pkgs; [
+        # python
+        uv
+        python312
+        # go
+        libcap
+        go
+        gcc
+        delve
+        # platformio
+        platformio-core
+        openocd
+        arduino-cli
+        avrdude
+        libftdi
+        libftdi1
+        libusb1
+      ]);
 
-    # NIX_HARDENING_ENABLE = "";
-    # LD_LIBRARY_PATH =
-    #   with pkgs;
-    #   lib.makeLibraryPath [
-    #     stdenv.cc.cc
-    #     zlib
-    #   ];
+    NIX_HARDENING_ENABLE = "";
+    LD_LIBRARY_PATH =
+      with pkgs;
+      lib.makeLibraryPath [
+        stdenv.cc.cc
+        zlib
+      ];
 
     runScript = ''
       ${pkgs.zsh}/bin/zsh
