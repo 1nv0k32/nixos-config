760f9664c21b9c8d030e5c73a2a6dc8ca4eab08c
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-01-06 14:33:18 +0100

    2026/01/06-14:33:18

--- system/mac/default.nix
+++ system/mac/default.nix
@@ -1,5 +1,11 @@
 { ... }:
 {
+  boot = {
+    extraModprobeConfig = ''
+      options hid_apple fnmode=3 iso_layout=-1 swap_fn_leftctrl=1 swap_opt_cmd=1
+    '';
+  };
+
   boot.loader.efi.canTouchEfiVariables = false;
   networking.networkmanager.enable = true;
 }
