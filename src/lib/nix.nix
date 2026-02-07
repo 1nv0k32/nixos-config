e2f6517be8a06e9628c6c0d8e2f81c8e32b3c480
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-02-08 00:46:27 +0100

    2026/02/08-00:46:27

--- src/lib/nix.nix
+++ src/lib/nix.nix
@@ -1,25 +1,22 @@
-{ lib, pkgs, ... }:
+{ ... }:
 {
   nix = {
     channel.enable = false;
     gc = {
       automatic = true;
       options = "--delete-older-than 3d";
-    }
-    // lib.mkIf (pkgs.stdenv.hostPlatform.system != "aarch64-darwin") {
       dates = "daily";
       persistent = true;
     };
 
     optimise = {
       automatic = true;
-    }
-    // lib.mkIf (pkgs.stdenv.hostPlatform.system != "aarch64-darwin") {
       dates = [ "daily" ];
       persistent = true;
     };
 
     settings = {
+      # auto-optimise-store = true;
       min-free = 512 * 1024 * 1024;
       max-free = 3000 * 1024 * 1024;
       download-buffer-size = 268435456;
@@ -30,9 +27,6 @@
         "nix-command"
         "flakes"
       ];
-    }
-    // lib.mkIf (pkgs.stdenv.hostPlatform.system != "aarch64-darwin") {
-      auto-optimise-store = true;
     };
   };
 }
