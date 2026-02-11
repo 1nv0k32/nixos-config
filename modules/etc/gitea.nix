028c6e98a1b8643c75cacaba660ae3761b80a2f9
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-02-11 23:04:17 +0100

    2026/02/11-23:04:17

--- modules/etc/gitea.nix
+++ modules/etc/gitea.nix
@@ -3,9 +3,30 @@
   services.gitea = {
     enable = true;
     settings = {
-      server.HTTP_ADDR = "0.0.0.0";
+      server.HTTP_ADDR = "127.0.0.1";
+      server.HTTP_PORT = 3000;
       server.DOMAIN = "${config.networking.fqdn}";
       webhook.ALLOWED_HOST_LIST = "*";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "git.${config.networking.fqdn}" = {
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3000";
+          extraConfig = ''
+            client_max_body_size 512M;
+            proxy_set_header Connection $http_connection;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
+      };
     };
   };
 }
