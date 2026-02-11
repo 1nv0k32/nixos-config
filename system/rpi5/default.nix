37b2803d3200e0b073b2088c51297ac3e3b0dda4
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-02-11 22:38:48 +0100

    2026/02/11-22:38:48

--- system/rpi5/default.nix
+++ system/rpi5/default.nix
@@ -14,4 +14,10 @@
   boot.loader.raspberry-pi.bootloader = "kernel";
 
   systemd.network.enable = true;
+
+  boot.kernelParams = [
+    "cgroup_enable=cpuset"
+    "cgroup_memory=1"
+    "cgroup_enable=memory"
+  ];
 }
