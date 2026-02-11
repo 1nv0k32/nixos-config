5f9d969111605bf59043686f70ece81917929234
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-02-11 21:38:10 +0100

    2026/02/11-21:38:10

--- modules/etc/media.nix
+++ modules/etc/media.nix
@@ -11,8 +11,6 @@ in
 
   systemd.tmpfiles.rules = [ "d ${media_dir} 0775 ${user} ${group}" ];
 
-  # networking.firewall.allowedUDPPorts = [ 5353 ];
-
   services = {
     minidlna = {
       enable = true;
