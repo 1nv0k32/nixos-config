61aa59be0d60df402ad4ea4595f928848a4a0889
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-02-11 22:31:12 +0100

    2026/02/11-22:31:12

--- system/rpi5/default.nix
+++ system/rpi5/default.nix
@@ -6,6 +6,7 @@
   imports = [
     (import ./disko.nix)
     (import "${self}/modules/etc/media.nix")
+    (import "${self}/modules/etc/k3s.nix")
   ];
 
   users.users.root.initialPassword = "root";
