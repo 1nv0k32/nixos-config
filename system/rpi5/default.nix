edd6cdbe3cf2bd03d0a001daa0fdc26ed461c4de
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-03-15 22:25:19 +0100

    2026/03/15-22:25:19

--- system/rpi5/default.nix
+++ system/rpi5/default.nix
@@ -12,15 +12,17 @@
 
   users.users.root.initialPassword = "root";
   systemd.network.enable = true;
-  networking.useDHCP = lib.mkForce false;
-  networking.firewall.enable = lib.mkForce false;
+  networking = {
+    useDHCP = lib.mkForce false;
+    firewall.enable = lib.mkForce false;
+  };
 
   boot = {
     loader.raspberry-pi.bootloader = "kernel";
     kernelParams = [
       "cgroup_enable=cpuset"
-      "cgroup_memory=1"
       "cgroup_enable=memory"
+      "cgroup_memory=1"
     ];
   };
 
