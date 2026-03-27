e5801768b90ca24d9e46bc776d3ff972500c5107
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-03-27 22:07:34 +0100

    2026/03/27-22:07:34

--- modules/gui/appimage.nix
+++ modules/gui/appimage.nix
@@ -1,16 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  nixpkgs.config.permittedInsecurePackages = [
-    "libsoup-2.74.3"
-  ];
   programs.appimage = {
     enable = true;
     binfmt = true;
-    package = pkgs.appimage-run.override {
-      extraPkgs = pkgs: [
-        pkgs.libsoup_2_4
-        pkgs.webkitgtk_4_1
-      ];
-    };
   };
 }
