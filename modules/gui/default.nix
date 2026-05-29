d8ea8678ce164be32edbf23597c69849b67e1c68
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-05-29 23:47:43 +0200

    2026/05/29-23:47:43

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -2,15 +2,15 @@
 {
   imports = [
     #./pkgs.nix
-    #./xdg.nix
-    #./chromium.nix
+    ./xdg.nix
+    ./chromium.nix
     ./gnome.nix
-    #./gns3.nix
-    #./localsend.nix
-    #./winbox.nix
-    #./wireshark.nix
-    #./steam.nix
-    #./appimage.nix
+    ./gns3.nix
+    ./localsend.nix
+    ./winbox.nix
+    ./wireshark.nix
+    ./steam.nix
+    ./appimage.nix
   ];
 
   environment.sysConf.gui.enable = true;
