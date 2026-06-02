2f87321eba38d1473abc7c7c7a6c3adefc87685e
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-06-02 16:10:54 +0200

    2026/06/02-16:10:54

--- system/z13g2/default.nix
+++ system/z13g2/default.nix
@@ -31,20 +31,20 @@
   services = {
     fprintd.enable = true;
     # power-profiles-daemon.enable = lib.mkForce false;
-    tlp = {
-      enable = true;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 90;
-        STOP_CHARGE_THRESH_BAT0 = 100;
-        # CPU_DRIVER_OPMODE_ON_AC = "active";
-        # CPU_DRIVER_OPMODE_ON_BAT = "active";
-        # CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        # CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-        # CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        # CPU_BOOST_ON_AC = 1;
-        # CPU_BOOST_ON_BAT = 0;
-      };
-    };
+    # tlp = {
+    #   enable = true;
+    #   settings = {
+    #     START_CHARGE_THRESH_BAT0 = 90;
+    #     STOP_CHARGE_THRESH_BAT0 = 100;
+    #     CPU_DRIVER_OPMODE_ON_AC = "active";
+    #     CPU_DRIVER_OPMODE_ON_BAT = "active";
+    #     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+    #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    #     CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+    #     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    #     CPU_BOOST_ON_AC = 1;
+    #     CPU_BOOST_ON_BAT = 0;
+    #   };
+    # };
   };
 }
