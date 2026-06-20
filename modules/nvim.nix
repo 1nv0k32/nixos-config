838c22dfd60ff64a7456a41b313d5e366be23933
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-06-20 19:54:39 +0200

    2026/06/20-19:54:39

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -156,7 +156,7 @@
               border = "rounded";
             };
           };
-          completion.menu.documentation.auto_show = true;
+          completion.documentation.auto_show = true;
         };
       };
       lsp = {
