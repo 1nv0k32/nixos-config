8b79fb37f7af0700428f14a7f552bebea2286b84
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-06-20 23:52:09 +0200

    2026/06/20-23:52:09

--- src/lib/options.nix
+++ src/lib/options.nix
@@ -10,7 +10,7 @@
       sshPubKeys = lib.mkOption {
         type = lib.types.listOf lib.types.singleLineStr;
         default = [
-          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINmmAvTaZ5rRNhY0mnX2UzUCX5pRX0DX+VCEBxCXbX9sAAAABnNzaDo1Yw== rick@nyx" # 5c
+          "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINGeXe8VqajOuRf+0LECYQWMmaIee5shvMgiq3XOLNvoAAAABnNzaDo1Yw== rick@nyx" # 5c
           "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICjS+ZPa4qUA+Vm0C7Br8PJ/U1Z9dh6bXcXZdko3ZFz9AAAAB3NzaDpiaW8= rick@nyx" # bio
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHn0AqWzb6VBqaJX7miDeuvxwlzfG3+UbKOyMlQQTz3r rick@nyx" # local
         ];
@@ -20,7 +20,7 @@
         type = lib.types.str;
         default =
           let
-            key_5c = "qJzOdjN/bRt9nqlPPpT3ULxN0omg9EgvhpiIc5/xeqY5dQrk+K2Sc6IGYqIXne3oWV9usY/pq6gxfTom42DtGg==,+HdbjR3Pp5SAhRiDFSkIiUWCtpTHv81c9fV7aNcnUdaiKVWMQbFsgpqsPsG0MG3dx76bCjB1QhOKxv6k7V4+QQ==,es256,";
+            key_5c = "b2iecx1K5cZe7Y0mK1QoSExxzd05RNkG8rQcY+2Eqi5PgrudhFVe3dJd3olkA6zh9mYrEfOHMaXsahFLauaXsA==,DsrO0lWQeBujzQj10hyPDiL0fgEdv+h+qoMuTVpmdc8osRjwFhGR3pGDtEycjKGOc2E58ZaSZ6stvQIDyre8CQ==,es256,";
             key_bio = "7wwRJCaiRcL/HwR8JK7hdITpvcsX55jKDtY7ckBhUB00p8Id2s5AykzVPq79dbKvkiQhhcrQvd3bT1w3x16Nyw==,ijaKeTEL3VG81mm+0zi0XxZe8uXbcR8j8Rr/oZZGn3FbpTlWm+5wRyYjx0hPz02Ve8QyspXg935VnHl8lRMahw==,es256,+presence";
           in
           ''
