d146c9356bdb5973225ce2a3aba5895365e95da3
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-06-20 20:53:55 +0200

    2026/06/20-20:53:55

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -53,12 +53,12 @@
       {
         mode = "v";
         key = "J";
-        action = "<CMD> m '>+1<CR>gv=gv";
+        action = ":m '>+1<CR>gv=gv";
       }
       {
         mode = "v";
         key = "K";
-        action = "<CMD> m '<-2<CR>gv=gv";
+        action = ":m '<-2<CR>gv=gv";
       }
       {
         mode = "n";
