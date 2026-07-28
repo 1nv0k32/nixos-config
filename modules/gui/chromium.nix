77d57e749cc807e73b5c6493d713c7cc82fa1c15
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-07-29 01:37:52 +0200

    2026/07/29-01:37:52

--- modules/gui/chromium.nix
+++ modules/gui/chromium.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    (pkgs.chromium.override {
+    (pkgs.unstable.chromium.override {
       commandLineArgs = [
         "--enable-features=AcceleratedVideoDecodeLinuxZeroCopyGL,AcceleratedVideoDecodeLinuxGL,VaapiIgnoreDriverChecks"
         "--ignore-gpu-blocklist"
