8d67b3277ceb3c7022a4576f35f26cba9734ee4a
Armin Mahdilou <Armin.Mahdilou@gmail.com>
2026-08-14 12:11:57 +0200

    2026/08/14-12:11:57

--- modules/tools/sdr.nix
+++ modules/tools/sdr.nix
@@ -1,4 +1,25 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  gui = config.environment.sysConf.gui.enable;
+in
+{
+  config = lib.mkIf (pkgs.stdenv.hostPlatform.system == "x86_64-linux") {
+    environment.systemPackages =
+      with pkgs;
+      [
+        uhd
+      ]
+      ++ lib.optionals gui [
+        gqrx
+      ];
 
+    services.udev.packages = with pkgs; [
+      uhd
+    ];
+  };
 }
